d0c89687f2e419d2d3947f1d4c20b96f0d7ae0a4
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-01 09:02:43 -0400

    added fetchPosts action and ThreadCard component

---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,31 @@
+interface Props {
+  id: string;
+  currentUserId: string;
+  parentId: string | null;
+  content: string;
+  author: {
+    name: string;
+    image: string;
+    id: string;
+  };
+  community: {
+    id: string;
+    name: string;
+    image: string;
+  } | null;
+  createdAt: string;
+  comments: {
+    author: {
+      image: string;
+    };
+  }[];
+  isComment?: boolean;
+}
+
+const ThreadCard = ({ id, currentUserId, parentId, content, author, community, createdAt, comments }: Props) => {
+  return (
+    <article></article>
+  )
+};
+
+export default ThreadCard;
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -32,3 +32,33 @@ export async function createThread({ text, author, communityId, path }: Params) 
     throw new Error(`Error creathing thread: ${error}`);
   }
 }
+
+export async function fetchPosts(pagenumber = 1, pageSize = 20) {
+  connectToDB();
+
+  // Calculate the number of posts to skip
+  const skipAmount = (pagenumber - 1) * pageSize;
+
+  // Fetch the posts that have no parents (top level threads)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User })
+    .populate({
+      path: 'children',
+      populate: {
+        path: 'author',
+        model: User,
+        select: '_id name parentId image',
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+}
