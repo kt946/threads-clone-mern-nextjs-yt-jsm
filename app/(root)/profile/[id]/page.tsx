ca054925f94c5544cdc0e045ddf55512528026aa
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-06 07:40:46 -0400

    added profile page and ProfileHeader component

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,30 @@
+import PostThread from '@/components/forms/PostThread';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import { fetchUser } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+async function Page({ params }: { params: { id: string } }) {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(params.id);
+
+  if (!userInfo?.onboarded) redirect('/onboarding');
+
+  return (
+    <section>
+      <ProfileHeader
+        accountId={userInfo.id}
+        authUserId={user.id}
+        name={userInfo.name}
+        username={userInfo.username}
+        imgUrl={userInfo.image}
+        bio={userInfo.bio}
+      />
+    </section>
+  );
+}
+
+export default Page;
