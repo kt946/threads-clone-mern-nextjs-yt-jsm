b36130a1327f75d2a95088eacd1e9d16c7ad5f7c
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-07 13:49:48 -0400

    added UserCard component

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -7,6 +7,7 @@ import { redirect } from 'next/navigation';
 import { profileTabs } from '@/constants';
 import Image from 'next/image';
 import ThreadsTab from '@/components/shared/ThreadsTab';
+import UserCard from '@/components/cards/UserCard';
 
 async function Page() {
   const user = await currentUser();
@@ -29,7 +30,24 @@ async function Page() {
 
       {/* Search Bar */}
 
-      <div></div>
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No Result</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
 }
