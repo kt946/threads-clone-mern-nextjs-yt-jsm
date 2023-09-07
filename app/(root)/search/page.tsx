df47dea59165b031b36b6a13cfab021f5f8cd831
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-07 13:33:30 -0400

    added search page and action to fetch all users

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,37 @@
+import PostThread from '@/components/forms/PostThread';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+import { profileTabs } from '@/constants';
+import Image from 'next/image';
+import ThreadsTab from '@/components/shared/ThreadsTab';
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect('/onboarding');
+
+  // Fetch users
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: '',
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+
+      {/* Search Bar */}
+
+      <div></div>
+    </section>
+  );
+}
+
+export default Page;
